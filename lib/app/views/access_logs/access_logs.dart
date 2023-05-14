545bed0afad61710ffc01a810b718e04150cadb3
Chrsitine23 <clange561@gmail.com>
2023-05-14 20:06:55 +0800

    life changes

--- lib/app/views/access_logs/access_logs.dart
+++ lib/app/views/access_logs/access_logs.dart
@@ -1,12 +1,314 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AccessLogsView extends StatelessWidget {
   const AccessLogsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Access Logs"),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              '150 total number of access Fireduino Logs',
+              style: TextStyle(
+                fontSize: 12,
+                height: 5
+              ),
+            ),
+            Container(
+              width: 350,
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 15, top: 10, bottom: 10, right: 10),
+                        child: const Column(
+                          children: [
+                            Text(
+                              'MAR',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(
+                              '18',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10,left: 10,bottom: 10, right: 20),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Fireduino 1',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 12,
+                                
+                              ),
+                            ),
+                            Text(
+                              'Last accessed 10 mins ago',
+                              style: TextStyle(
+                                fontSize: 10
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Maverick Fabroa',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            Container(
+              child: const Text(''),
+            ),
+
+            Container(
+              width: 350,
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 15, top: 10, bottom: 10, right: 10),
+                        child: const Column(
+                          children: [
+                            Text(
+                              'MAR',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(
+                              '18',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10,left: 10,bottom: 10, right: 20),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Fireduino 2',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 12,
+                                
+                              ),
+                            ),
+                            Text(
+                              'Last accessed 54 mins ago',
+                              style: TextStyle(
+                                fontSize: 10
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Christine Lange',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            Container(
+              child: const Text(''),
+            ),
+
+            Container(
+              width: 350,
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 15, top: 10, bottom: 10, right: 10),
+                        child: const Column(
+                          children: [
+                            Text(
+                              'MAR',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(
+                              '18',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10,left: 10,bottom: 10, right: 20),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Fireduino 3',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 12,
+                                
+                              ),
+                            ),
+                            Text(
+                              'Last accessed 2 hours ago',
+                              style: TextStyle(
+                                fontSize: 10
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Maverick Fabroa',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            Container(
+              child: const Text(''),
+            ),
+
+            Container(
+              width: 350,
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 15, top: 10, bottom: 10, right: 10),
+                        child: const Column(
+                          children: [
+                            Text(
+                              'MAR',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            Text(
+                              '18',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10,left: 10,bottom: 10, right: 20),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Fireduino 4',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 12,
+                                
+                              ),
+                            ),
+                            Text(
+                              'Last accessed 5 hours ago',
+                              style: TextStyle(
+                                fontSize: 10
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Jewel Cedrick Gesim',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/app/views/login_history/login_history.dart
+++ lib/app/views/login_history/login_history.dart
@@ -5,8 +5,205 @@ class LoginHistoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Login History"),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Recent login by Maverick Fabroa (10 mins ago)',
+              style: TextStyle(
+                fontSize: 12,
+                height: 5
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: new NetworkImage('https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2018/09/08/Pictures/_7ae928c8-b34e-11e8-bb15-a1f88311a832.jpg'),
+                  ),
+                  Column(
+                    children: [
+                       Container(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: const Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                             Text(
+                              'Maverick Fabroa',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            
+                        Text(
+                        'Last accessed 10 mins ago',
+                        style: TextStyle(
+                          fontSize: 10
+                        ),
+                      )
+                          ],
+                        ),
+                       ),
+                      
+                    ],
+                  )
+                ],
+              ),
+
+            ),
+            
+            Container(
+              child: const Text(''),
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: new NetworkImage('https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2018/09/08/Pictures/_7ae928c8-b34e-11e8-bb15-a1f88311a832.jpg'),
+                  ),
+                  Column(
+                    children: [
+                       Container(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: const Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                             Text(
+                              'Christine Lange',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            
+                        Text(
+                        'Last accessed 54 mins ago',
+                        style: TextStyle(
+                          fontSize: 10
+                        ),
+                      )
+                          ],
+                        ),
+                       ),
+                      
+                    ],
+                  )
+                ],
+              ),
+
+            ),
+
+            Container(
+              child: const Text(''),
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: new NetworkImage('https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2018/09/08/Pictures/_7ae928c8-b34e-11e8-bb15-a1f88311a832.jpg'),
+                  ),
+                  Column(
+                    children: [
+                       Container(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: const Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                             Text(
+                              'Christine Lange',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            
+                        Text(
+                        'Last accessed 2 hours ago',
+                        style: TextStyle(
+                          fontSize: 10
+                        ),
+                      )
+                          ],
+                        ),
+                       ),
+                      
+                    ],
+                  )
+                ],
+              ),
+
+            ),
+
+            Container(
+              child: const Text(''),
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: new NetworkImage('https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2018/09/08/Pictures/_7ae928c8-b34e-11e8-bb15-a1f88311a832.jpg'),
+                  ),
+                  Column(
+                    children: [
+                       Container(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: const Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                             Text(
+                              'Jewel Cedrick Gesim',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            
+                        Text(
+                        'Last accessed 5 hours ago',
+                        style: TextStyle(
+                          fontSize: 10
+                        ),
+                      )
+                          ],
+                        ),
+                       ),
+                      
+                    ],
+                  )
+                ],
+              ),
+
+            )
+          ],
+        ),
+      ),
     );
   }
 }
